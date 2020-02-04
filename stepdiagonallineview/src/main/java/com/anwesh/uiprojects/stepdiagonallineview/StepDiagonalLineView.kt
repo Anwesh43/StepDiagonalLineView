ae7ebbcaf0da8597172f4c55e0ccac9c7090dbf0
Anwesh43 <anweshthecool0@gmail.com>
2020-02-05 02:12:19 +0530

    adding StepDiagonalLineView class

--- stepdiagonallineview/src/main/java/com/anwesh/uiprojects/stepdiagonallineview/StepDiagonalLineView.kt
+++ stepdiagonallineview/src/main/java/com/anwesh/uiprojects/stepdiagonallineview/StepDiagonalLineView.kt
@@ -58,3 +58,21 @@ fun Canvas.drawDLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class StepDiagonalLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
