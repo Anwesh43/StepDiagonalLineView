f2ff94f5eade803d0d4a6dcc8e4ea5b8f8fcdd82
Anwesh43 <anweshthecool0@gmail.com>
2020-02-05 01:36:37 +0530

    adding constants and libraries

--- stepdiagonallineview/src/main/java/com/anwesh/uiprojects/stepdiagonallineview/StepDiagonalLineView.kt
+++ stepdiagonallineview/src/main/java/com/anwesh/uiprojects/stepdiagonallineview/StepDiagonalLineView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.stepdiagonallineview
 /**
  * Created by anweshmishra on 04/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 5
+val scGap : Float = 0.02f / lines
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
