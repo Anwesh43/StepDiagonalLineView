3969c9ee48d57a212e12fceb1d3f1f8ba404f703
Anwesh43 <anweshthecool0@gmail.com>
2020-02-05 02:52:01 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedstepdiagonallineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepdiagonallineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedstepdiagonallineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.stepdiagonallineview.StepDiagonalLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepDiagonalLineView.create(this)
     }
 }
