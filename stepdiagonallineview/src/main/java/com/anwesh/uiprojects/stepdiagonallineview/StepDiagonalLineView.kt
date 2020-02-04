6d96130beae601ad5aedc211d04930868efadecd
Anwesh43 <anweshthecool0@gmail.com>
2020-02-05 01:38:36 +0530

    adding Int and Float inverse methods

--- stepdiagonallineview/src/main/java/com/anwesh/uiprojects/stepdiagonallineview/StepDiagonalLineView.kt
+++ stepdiagonallineview/src/main/java/com/anwesh/uiprojects/stepdiagonallineview/StepDiagonalLineView.kt
@@ -19,3 +19,8 @@ val strokeFactor : Float = 90f
 val delay : Long = 20
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
